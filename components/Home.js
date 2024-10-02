5978fbc93601a01bea27f9a4739c8cd273e771b1
Your name <Your email>
2024-10-02 16:15:10 +0200

    commit

--- components/Home.js
+++ components/Home.js
@@ -36,7 +36,7 @@ function Home() {
 
   
   useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('https://my-moviz-front-chi.vercel.app/movies')
       .then(response => response.json())
       .then(data => {
         const formattedData = data.movies.map(movie => {
